1344a41fc3b964102a4b7d5faabdea502963ae4c
LucasMedeiros-dev <lucasm676@gmail.com>
2023-08-29 17:42:06 -0300

    Adicionada as atividades 5 e 6

---
+++ Exercicios/Atividades_dia_02/atividade_05.py
@@ -0,0 +1,4 @@
+# Usando o for faça o print que vai de 0 a 10
+
+for i in range(0,11):
+    print(i)
---
+++ Exercicios/Atividades_dia_02/atividade_06.py
@@ -0,0 +1,6 @@
+# Faça um for que vai de 0 a 30 , mas pare no 20
+for i in range(0,31):
+    if i == 20:
+        print("Encontrado o 20, parando.")
+        break
+    print(i)
