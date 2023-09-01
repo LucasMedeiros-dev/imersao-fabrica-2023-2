ab90fa8ca414afde6596f60a71fc0ccbe6cd9f64
LucasMedeiros-dev <lucasm676@gmail.com>
2023-09-01 16:47:26 -0300

    Limpando o repo

--- Exercicios/Atividades_dia_04/projeto_django/agenda/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('agenda/', include("apps.todo.urls")),
-    path('usuarios/', include("apps.users.urls"))
-]
--- Exercicios/Atividades_dia_04/projeto_django/apps/todo/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from apps.todo.models import Task
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ['id', 'name', 'description', 'date', 'user']
